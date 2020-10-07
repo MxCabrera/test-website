796c497ed433a6c248dc47df48d4274fa8425fe8
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-10-07 00:15:48 -0400

    decided to make this into a chuck norris joke app, connected API using Ajax call, doc ready function to create random list of jokes, button that is connected to display new joke to the DOM.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('hello miguel');
+const jokeApi = {};
+
+jokeApi.apiUrl =  `http://api.icndb.com/jokes/random`
+
+jokeApi.data = [];
+
+jokeApi.getApiData = () => {
+   $.ajax({
+      url: jokeApi.apiUrl,
+      method: 'GET',
+      dataType: 'json',
+   }).then((results)=> {
+      jokeApi.data.push(results.value)
+      console.log('it worked', jokeApi.data[0].joke)
+      // console.log(jokeApi.data)
+
+      // jokeApi.getRandom()
+   })
+   
+}
+
+$('.newJoke').on('click', function(){
+   for (let i=0; i < jokeApi.data.length; i++) {
+      $('.funnyJoke').html(`${jokeApi.data[i].joke}`);
+   }
+   jokeApi.getApiData()
+})
+
+$(function() {
+   jokeApi.getApiData();
+});
