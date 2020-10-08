352ab96a4859ea9fbb75859abd4acba0899fda8f
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-10-07 20:33:30 -0400

    was able to to create an array that shows a random image every time fact button is clicked

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const jokeApi = {};
 jokeApi.apiUrl =  `http://api.icndb.com/jokes/random`
 
 jokeApi.data = [];
+jokeApi.pics = [
+   `./images/chuck2.jpg`,
+   `./images/chuck3.webp`,
+   `./images/chuck4.jpg`,
+   `./images/chuck5.jpg`,
+   `./images/chuck6.jpg`,
+   `./images/chuck7.jpg`,
+   `./images/chuck8.jpg`,
+
+]
 
 jokeApi.getApiData = () => {
    $.ajax({
@@ -11,12 +21,22 @@ jokeApi.getApiData = () => {
       dataType: 'json',
    }).then((results)=> {
       jokeApi.data.push(results.value)
-      console.log('it worked', jokeApi.data[0].joke)
-      // console.log(jokeApi.data)
+      // console.log('it worked', jokeApi.data[0].joke)
 
-      // jokeApi.getRandom()
    })
-   
+}
+
+
+// console.log(jokeApi.pics)
+
+jokeApi.getRandom = () => {
+   let pics = jokeApi.pics[Math.floor(Math.random() * jokeApi.pics.length)]
+   $('.newPicz').html(`
+   <div class="imgBox">
+      <img src="${pics}" alt="chuck norris">
+   </div> 
+   `)
+   console.log(pics)
 }
 
 $('.newJoke').on('click', function(){
@@ -24,6 +44,7 @@ $('.newJoke').on('click', function(){
       $('.funnyJoke').html(`${jokeApi.data[i].joke}`);
    }
    jokeApi.getApiData()
+   jokeApi.getRandom()
 })
 
 $(function() {
